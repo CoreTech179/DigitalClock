5ffe50cbf3516c8578e7dd9954a1cbb2931510ec
Deep <shyamaly21l@gmail.com>
2024-01-26 22:54:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+
+  let time = new Date().toLocaleTimeString();
+  
+  const [currentTime, setCurrentTime] = useState(time);
+  
+  const updateTime = ()=>{
+    
+    time = new Date().toLocaleTimeString();
+
+    setCurrentTime(time);
+  }
+
+  setInterval(updateTime,1000);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <h3>{currentTime}</h3>
+      </div>
+    </>
   );
 }
 
